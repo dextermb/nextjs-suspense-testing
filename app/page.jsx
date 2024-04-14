41b24ecd7c27054b6eb7fac2ddc063bf28cdef78
Dexter Marks-Barber <dexter@marks-barber.co.uk>
2024-04-14 14:35:59 +0100

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import { Suspense } from "react";
 import { NestedComponent } from "./nested-component";
 import { LoadingNestedComponent } from "./loading-nested-component";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <>
